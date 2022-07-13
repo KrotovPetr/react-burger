3b61ec663ec9acb036ea403eeeb1b2c7002640b7
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-07-13 23:02:43 +0300

    Last commit to sprint-5/step-1???

--- src/pages/Order/order.tsx
+++ src/pages/Order/order.tsx
@@ -1,17 +1,25 @@
 import React, { FC, useEffect, useState } from 'react';
 import orderStyle from './order.module.css';
 import OrderIngredients from '../../Components/orderIngredients/order-ingredients.';
-import { useDispatch } from '../../utils/types/store';
+import { useDispatch, useSelector } from '../../utils/types/store';
 import {
     WS_CONNECTION_CLOSED,
     WS_CONNECTION_START,
 } from '../../Services/actions/socketActions';
 const Order: FC = () => {
     const dispatch = useDispatch();
+    const { ordersActive, personOrdersActive } = useSelector((store) => ({
+        ordersActive: store.requests.ordersActive,
+        personOrdersActive: store.requests.personOrdersActive,
+    }));
     // console.log('order')
     useEffect(() => {
-        dispatch({ type: WS_CONNECTION_START, payload: '/all' });
+        // console.log('order-open');
+        !ordersActive &&
+            !personOrdersActive &&
+            dispatch({ type: WS_CONNECTION_START, payload: '/all' });
         return () => {
+            // console.log('order closed');
             // console.log('закрываем');
             dispatch({ type: WS_CONNECTION_CLOSED });
         };
